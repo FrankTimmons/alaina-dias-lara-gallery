8601133ef3e11a02cf6f1db7dfdf6ee0ff5e382c
FrankTimmons <franktimmonspdx@gmail.com>
2023-05-17 13:31:02 -0700

    Have the header link back to the home page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex flex-col h-100 pt-20 pb-10 border-b-2 border-black mb-10 font-lobster'>
-      <h1 className='text-6xl'>Alaina Dias Lara</h1>
-      <h1 className='text-3xl ml-24'>Interdisciplinary Artist</h1>
-    </div>
+    <Link href={'/'}>
+      <div className='flex flex-col h-100 pt-20 pb-10 border-b-2 border-black mb-10 font-lobster'>
+        <h1 className='text-6xl'>Alaina Dias Lara</h1>
+        <h1 className='text-3xl ml-24'>Interdisciplinary Artist</h1>
+      </div>
+    </Link>
   )
 }
 
