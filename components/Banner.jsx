19cd61b8cd3f91b4f1ae0e2e9bfc81e474b0b253
FrankTimmons <franktimmonspdx@gmail.com>
2023-05-16 21:20:56 -0700

    Add quote to Banner component

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Banner = ({image}) => {
   return (
     <div className='flex flex-col items-center'>
-      <img className='h-auto w-[50%]' src={image}/>
+      <img className='h-auto w-full' src={image}/>
+      <h1 className='text-center mt-10'>
+        “Life is a balance between transformation and resistance to transformation. Think of yourself as a dancer on the blurry edge separating order from chaos.”
+      </h1>
+      <h1>- Fred Alan Wolf, Ph.D.</h1>
     </div>
   )
 }
