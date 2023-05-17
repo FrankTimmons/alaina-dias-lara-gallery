0be73b236f616aa365ecfafb39c585acecdc6b1b
FrankTimmons <franktimmonspdx@gmail.com>
2023-05-16 21:20:38 -0700

    Add Lobster Font

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex flex-column h-100 py-20 border-b-2 border-black mb-10'>
-      <h1 className='text-8xl'>Alaina Nearing</h1>
+    <div className='flex flex-col h-100 pt-20 pb-10 border-b-2 border-black mb-10 font-lobster'>
+      <h1 className='text-6xl'>Alaina Dias Lara</h1>
+      <h1 className='text-3xl ml-24'>Interdisciplinary Artist</h1>
     </div>
   )
 }
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,8 +6,11 @@ const Layout = ({children}) => {
     <div>
       <Head>
         <title>Alaina Nearing</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="styles.css" />
       </Head>
-      <main className='mx-[200px]'>
+      <main className='2xl:mx-[30%] xl:mx-[25%] lg:mx-[20%] mx-[10%]'>
         {children}
       </main>
     </div>
