9cd2dc5d5b3834ee850af87212c43b4e85cdeb42
FrankTimmons <franktimmonspdx@gmail.com>
2023-05-16 20:56:13 -0700

    Set up Header Component

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex flex-column h-100 py-20 border-b-2 border-black mb-10'>
+      <h1 className='text-8xl'>Alaina Nearing</h1>
+    </div>
+  )
+}
+
+export default Header
