51be42f5c28ab54825a077b7b6eb0a5f80110e52
FrankTimmons <franktimmonspdx@gmail.com>
2023-05-29 15:39:09 -0700

    Modify Navbar component with a dropdown and move the outline from Header to Navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
   return (
     <Link href={'/'}>
-      <div className='flex flex-col h-100 pt-20 pb-10 border-b-2 border-black mb-10 font-lobster'>
+      <div className='flex flex-col h-100 pt-10 pb-10 font-lobster'>
         <h1 className='text-6xl'>Alaina Dias Lara</h1>
         <h1 className='text-3xl ml-24'>Interdisciplinary Artist</h1>
       </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,25 @@
 import Link from 'next/link'
-import React from 'react'
+import React, {useState} from 'react'
 
 const Navbar = ({galleries}) => {
-  return (
-    <div>
-      {galleries.map((gallery) => 
-        <Link href={`/gallery/${gallery.slug.current}`}>
-          <div>
-            {gallery.gallery}
-          </div>
-        </Link>
-      )}
+  const [galleriesDropdown, setGalleriesDropdown] = useState(false)
+
+
+  return ( 
+    <div className='flex justify-end border-b-2 border-black mb-10'>
+      <div className='ml-5'>
+        <h1 onClick={() => setGalleriesDropdown(!galleriesDropdown)} className='hover:text-red-400'>GALLERIES</h1>
+        <div className={galleriesDropdown ? 'absolute flex flex-col bg-slate-300' : 'hidden'}>
+          {galleries.map((gallery) => 
+            <Link href={`/gallery/${gallery.slug.current}`} className='hover:text-red-400'>
+              {gallery.gallery.toUpperCase()}
+            </Link>
+          )}
+        </div>
+      </div>
+      <div className='ml-5 hover:text-red-400'>
+        BLOG
+      </div>
     </div>
   )
 }
