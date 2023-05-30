53c852638ef58216d7264c8b1a2565ba4a59f935
FrankTimmons <franktimmonspdx@gmail.com>
2023-05-30 16:38:58 -0700

    Move Paintings to their own component, and add smooth fade in on load

---
+++ components/Painting.jsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from "react";
+import {urlFor} from '../lib/client'
+
+const Painting = ({painting, onClick}) => {
+  const [style, setStyle] = useState("h-[300px] w-[300px] object-contain opacity-0");
+  useEffect(() => {
+    setStyle("h-[300px] w-[300px] object-contain opacity-0")
+  }, [painting]);
+  return (
+    <div
+      key={painting.name}
+      className="mt-10 flex flex-col items-center content-center justify-center"
+      onClick={onClick}
+    >
+      <img
+        onLoad={() => setStyle('h-[300px] w-[300px] object-contain animate-imgFadeIn fill-mode-forwards')}
+        className={style}
+        src={urlFor(painting.image)}
+      />
+      <p className="text-xl">{painting.name}</p>
+      <p>{painting.dimensions}</p>
+    </div>
+  );
+};
+
+export default Painting;
--- pages/gallery/[slug].js
+++ pages/gallery/[slug].js
@@ -1,13 +1,15 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {client, urlFor} from '../../lib/client'
 import Header from '@/components/Header'
 import Navbar from '@/components/Navbar'
 import GalleryView from '@/components/GalleryView'
+import Painting from '@/components/Painting'
 
 const GalleryDetails = ({gallery, galleries}) => {
   const [galleryView, setGalleryView] = useState(false);
   const [currentPainting, setCurrentPainting] = useState(0);
-  
+  const [rerender, setRerender] = useState(false);
+
   return (
     <>
       <Header/>
@@ -15,22 +17,24 @@ const GalleryDetails = ({gallery, galleries}) => {
       <div className='text-2xl text-center'>{gallery.gallery.toUpperCase()}</div>
       <div className='grid grid-cols-3 gap-4 justify-center'>
         {gallery.paintings.map((painting, index) =>
-          <div 
-            key={painting._id} 
-            className='mt-10 flex flex-col items-center content-center justify-center' 
+          <Painting 
+            painting={painting} 
             onClick={() => {
-              setCurrentPainting(index); 
-              setGalleryView(true); 
-              console.log(galleryView);
-              console.log(currentPainting);
+              setCurrentPainting(index);
+              setGalleryView(true);
             }}
-          >
-            <img className='h-[300px] w-[300px] object-contain' src={urlFor(painting.image)}/>
-            <p className='text-xl'>{painting.name}</p>
-            <p>{painting.dimensions}</p>
-          </div>
+          />
         )}
-        {galleryView ? <GalleryView gallery={gallery} currentPainting={currentPainting} setCurrentPainting={setCurrentPainting} closeGalleryView={()=>setGalleryView(false)}/> : <></>}
+        {galleryView ? 
+          <GalleryView 
+            gallery={gallery} 
+            currentPainting={currentPainting} 
+            nextPainting={() => setCurrentPainting(currentPainting+1)} 
+            previousPainting={()=>setCurrentPainting(currentPainting-1)} 
+            closeGalleryView={()=>setGalleryView(false)}/> 
+          : 
+          <></>
+        }
       </div>
     </>
   )
