115e85a1b4a65f887a8bfc6f99e869e83c716f6e
FrankTimmons <franktimmonspdx@gmail.com>
2023-05-30 18:38:22 -0700

    Add fade in for front page image on the Banner Component

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { urlFor } from '../lib/client';
 
 const Banner = ({banner}) => {
+  const [style, setStyle] = useState("h-auto w-full opacity-0");
+
   return (
     <div className='flex flex-col items-center'>
-      <img className='h-auto w-full' src={urlFor(banner.image)}/>
+      <img onLoad={() => setStyle('h-auto w-full animate-imgFadeIn fill-mode-forwards')} className={style} src={urlFor(banner.image)}/>
       <h1 className='text-center my-10'>
         {banner.quote}
       </h1>
