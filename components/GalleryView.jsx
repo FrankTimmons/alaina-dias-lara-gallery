eed100fdb98e5fc09a548f34afaf6465f26c3301
FrankTimmons <franktimmonspdx@gmail.com>
2023-05-30 20:40:03 -0700

    Complete base GalleryView functionality

--- components/GalleryView.jsx
+++ components/GalleryView.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import {client, urlFor} from '../lib/client'
+import { AiFillCaretRight, AiFillCaretLeft, AiOutlineClose } from "react-icons/ai";
 
 const GalleryView = ({currentPainting, gallery, closeGalleryView, nextPainting, previousPainting}) => {
   return (
-    <div onClick={closeGalleryView} className='fixed left-0 right-0 top-0 bottom-0 w-full h-full bg-black/90 flex items-center justify-center'>
-      <img className='w-auto h-[90%]' src={urlFor(gallery.paintings[currentPainting].image)} alt="" />
+    <div className='fixed left-0 right-0 top-0 bottom-0 w-full h-full flex items-center justify-center bg-black/90'>
+      <AiOutlineClose className='fixed top-2 right-2 text-white w-12 h-auto z-10' onClick={closeGalleryView}/>
+      <div className='fixed left-0 w-[50%] h-full' onClick={currentPainting == 0 ? null : previousPainting}>
+        {currentPainting == 0 ? <></> :
+          <AiFillCaretLeft className='fixed left-0 top-[50%] text-white w-[5%] h-auto'/>
+        }  
+      </div>
+      <div className='w-[90%] h-[90%] flex justify-center'>
+        <img className='object-contain' src={urlFor(gallery.paintings[currentPainting].image)} alt="" />
+      </div>
+      <div className='fixed right-0 w-[50%] h-full' onClick={currentPainting == (gallery.paintings.length - 1) ? null : nextPainting}>
+        {currentPainting == (gallery.paintings.length - 1) ? <></> :
+          <AiFillCaretRight className='fixed right-0 top-[50%] text-white w-[5%] h-auto'/>
+        }  
+      </div>
     </div>
   )
 }
