6e7583c3db05c76d86ea849c537ffcfafd2a954a
FrankTimmons <franktimmonspdx@gmail.com>
2023-05-31 14:53:19 -0700

    Add blog post schema to sanity

---
+++ sanity_backend/schemas/blog.js
@@ -0,0 +1,34 @@
+export default {
+  name: 'blogPost',
+  type: 'document',
+  title: 'Blog Post',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    },
+    {
+      name: 'date',
+      type: 'date',
+      title: 'Date'
+    },
+    {
+      name: 'post',
+      type: 'text',
+      title: 'Post'
+    },
+    {
+      name: 'pictures',
+      title: 'Pictures',
+      type: 'array',
+      of: [
+        {      
+          type: "image", 
+          title: 'Image', 
+          name: "image"
+        },
+      ]
+    }
+  ]
+}
