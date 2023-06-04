27ed55a6b7b0817a05df1d47089defcc0bb9379f
FrankTimmons <franktimmonspdx@gmail.com>
2023-06-03 17:09:30 -0700

    Delete Layout component in favor of _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
-import Layout from '@/components/Layout'
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return ( 
-    <Layout>
+    <>
+      <Head>
+          <title>Alaina Dias Lara</title>
+      </Head>
       <Component {...pageProps} />
-    </Layout>
+    </>
   )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="styles.css" />
+      </Head> 
+      <body className='bg-stone-100 min-h-screen'>
         <Main />
         <NextScript />
       </body>
