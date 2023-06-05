05ef4aceaf2dc157013d03e837a34dc5604d80d5
FrankTimmons <franktimmonspdx@gmail.com>
2023-06-05 16:35:54 -0700

    Add Logo SVG to the front page Banner component using newly installed SVGR library

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import { urlFor } from '../lib/client';
 import ImageFadeIn from "react-image-fade-in";
+import Logo from '../public/logo.svg'
 
 const Banner = ({banner}) => {
   return (
     <div className='flex flex-row items-center'>
-      <div className='flex justify-center items-center'>
-
+      <div className='flex flex-col justify-center items-center'>
+        <Logo/>
         <h1 className='text-center my-10 w-[60%]'>
           {banner.quote}
         </h1>
