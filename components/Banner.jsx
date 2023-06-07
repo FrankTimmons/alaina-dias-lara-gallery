f339626a6e47918ea2f35b2801d3c107b18a0204
FrankTimmons <franktimmonspdx@gmail.com>
2023-06-07 11:25:26 -0700

    Add an html parser package to allow bolding of the quote on the Banner component from Sanity

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,15 +1,17 @@
 import React, {useEffect, useState} from 'react'
 import { urlFor } from '../lib/client';
+import parse from 'html-react-parser';
 import ImageFadeIn from "react-image-fade-in";
 import Logo from '../public/logo.svg'
 
 const Banner = ({banner}) => {
   return (
     <div className='flex flex-row items-center'>
-      <div className='flex flex-col justify-center items-center'>
-        <Logo/>
-        <h1 className='text-center my-10 w-[60%]'>
-          {banner.quote}
+      <div className='flex flex-col justify-center items-center gap-24'>
+        <Logo className='h-[90%]'/>
+        <div className='bg-[#BC693A] w-full h-14'/>
+        <h1 className='my-10 w-[60%] font-serif text-xl'>
+          {parse(banner.quote)}
         </h1>
       </div>
       <img
