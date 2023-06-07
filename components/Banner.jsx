13276c6bccf1cbdc3eada4102aed2240895f39ef
FrankTimmons <franktimmonspdx@gmail.com>
2023-06-07 15:38:36 -0700

    Make small styling changes to Banner component

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -11,27 +11,27 @@ const Banner = ({banner}) => {
         <div className='flex justify-center items-center xl:h-[60%] lg:h-[50%]'>
           <Logo className='2xl:h-[50%] h-[75%] w-auto'/>
         </div>
-        <div className='bg-[#BC693A] w-full h-[5%]'/>
+        <div className='bg-[#BC693A] w-full h-[4%]'/>
         <div className='flex flex-col justify-center items-center content-center h-[35%]'>
           <h1 className='w-[60%] font-serif xl:text-2xl lg:text-lg'>
             {parse(banner.quote)}
           </h1>
-          <h1 className='w-[60%] font-serif xl:text-2xl text-xl text-right mt-6'>
+          <h1 className='w-[60%] font-serif xl:text-2xl lg:text-lg text-right mt-5'>
             - {banner.author}
           </h1>
         </div>
       </div>
-      <div className='min-[1150px]:w-[160%]'>
+      <div className='min-[1150px]:w-[150%] '>
         <img
           className='object-contain'
           src={urlFor(banner.image)}
         />
       </div>
       <div className='lg:hidden flex flex-col justify-center items-center h-[35%] my-12'>
-        <h1 className='w-[60%] font-serif xl:text-2xl text-xl'>
+        <h1 className='w-[60%] font-serif text-xl'>
           {parse(banner.quote)}
         </h1>
-        <h1 className='w-[60%] font-serif xl:text-2xl text-xl text-right mt-6'>
+        <h1 className='w-[60%] font-serif text-xl text-right mt-6'>
           - {banner.author}
         </h1>
       </div>
