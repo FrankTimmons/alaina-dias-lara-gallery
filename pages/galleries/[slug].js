1c57238f0a8574dbe45bcf51c21a024eec2c03f8
FrankTimmons <franktimmonspdx@gmail.com>
2023-07-20 15:08:34 -0700

    Import useState

--- pages/galleries/[slug].js
+++ pages/galleries/[slug].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { client, urlFor } from "../../lib/client";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
