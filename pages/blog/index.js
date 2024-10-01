817dd6a9704b6c177dfeec0d491c284befa53bc1
FrankTimmons <franktimmonspdx@gmail.com>
2024-09-30 21:45:15 -0700

    Work on compacting the blog page.

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { client, urlFor } from "../../lib/client";
 import BlogPost from "@/components/BlogPost";
 import ImageFadeIn from "react-image-fade-in";
 
 export default function BlogPosts({ frontPage, blogPosts }) {
+  const [currentPost, setCurrentPost] = useState(0);
+  const nextPost = () => setCurrentPost(currentPost + 1);
+  const prevPost = () => setCurrentPost(currentPost - 1);
 
   return (
     <>
@@ -16,10 +19,18 @@ export default function BlogPosts({ frontPage, blogPosts }) {
       <div className="fixed top-[125px] text-center inset-x-0 -z-10 bg-orange-100/80 mx-auto w-fit">
         <h1 className="text-5xl font-bold text-center p-6">perspectives</h1> 
       </div>
-      <div className="flex px-4 bg-orange-50 mt-[300px] min-h-screen py-6 flex-col items-center overflow-hidden">
-        {blogPosts.map((post, index) => (
-          <BlogPost key={index} post={post} />
-        ))}
+      <div className="flex px-4 bg-orange-50 mt-[300px] min-h-screen py-6 flex-col items-center overflow-hidden">        
+          <div className="flex justify-between px-6 lg:w-2/3 w-[90%]">
+            {currentPost != 0 ? 
+              <div onClick={prevPost} className="cursor-pointer hover:underline">Previous Post</div>
+              : <div className="h-2 w-2 bg-orange-50"/>
+            }
+            {currentPost != (blogPosts.length - 1) ?        
+              <div onClick={nextPost} className="cursor-pointer hover:underline">Next Post</div>
+              : <div className="h-2 w-2 bg-orange-50"/>
+            }
+          </div>
+          <BlogPost key={currentPost} post={blogPosts[currentPost]} />
       </div>
     </>
   );
